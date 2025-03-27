224d273e8b8b336dbeb2f1a1d3fd1759df0b042f
DESKTOP-52D78OC\LENOVO <tanieska98@gmail.com>
2025-03-26 20:48:41 -0600

    init structura for spring project

---
+++ src/test/java/com/ac/springbootweb/SpringBootWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ac.springbootweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
